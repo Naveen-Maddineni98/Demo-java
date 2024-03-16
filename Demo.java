0512d2e096611d6cc9ba8f6f9a1e257af30dfdf3
Naveen Maddineni <m.navein6@gmail.com>
2024-03-16 01:10:13 -0500

    committing Demo

---
+++ Demo.java
@@ -0,0 +1,26 @@
+public class Demo{
+    public static void main(String args[])
+    {
+        int nums[][]= new int [4][5];
+        
+      for(int i=0;i<=3;i++)
+      {
+        for (int j=0;j<=4;j++)
+        {
+            System.out.print(nums [i][j]+" ");
+        }
+        System.out.println();
+      }
+    }
+}
+
+
+
+
+
+
+        
+
+        
+
+    
